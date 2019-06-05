1f064817d3a7a68ba9b94aca50aef8e88c9f1a59
Konrad Pieprzyca <konrad.pieprzyca@pwc.com>
2019-06-05 16:29:50 +0200

    f1 branch changes

--- develop.js
+++ develop.js
@@ -1,6 +1,8 @@
 //this is new line added to develop
 //line2
-//line3
+//line3 //f1 added
 //line4
+//line7 //f1 added
 //line5
 //line6
+//line8
---
+++ f1.js
@@ -0,0 +1,7 @@
+//f1 branch created
+//line1
+//line2
+//line3
+//line4
+//line5
+//line6
